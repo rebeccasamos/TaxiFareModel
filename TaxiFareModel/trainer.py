7aeb6ebc5cb5858539ebf195efa64f4a0c9e2f01
Rebecca Samos <rebecca.samos@googlemail.com>
2022-02-22 15:09:17 +0000

    third challenge

---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,65 @@
+# imports
+from locale import YESEXPR
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+from TaxiFareModel.data import clean_data, get_data
+from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+from TaxiFareModel.utils import compute_rmse
+from sklearn.model_selection import train_test_split
+
+
+class Trainer():
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+
+    def set_pipeline(self):
+        """defines the pipeline as a class attribute"""
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+        self.pipeline = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
+
+    def run(self):
+        """set and train the pipeline"""
+        self.set_pipeline()
+        self.pipeline.fit(self.X, self.y)
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        y_pred = self.pipeline.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+        return rmse
+
+
+if __name__ == "__main__":
+    df = get_data()
+    cleaned_df = clean_data(df)
+    X = cleaned_df.drop('fare_amount', axis = 1)
+    y = cleaned_df['fare_amount']
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3)
+    trainer = Trainer(X_train, y_train)
+    trainer.run()
+    result = trainer.evaluate(X_test, y_test)
+    print(result)
